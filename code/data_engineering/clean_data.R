0343a7beb3c5366ce6ef49cbc77090888108a6a6
mawi <matthias.widmer@bfs.admin.ch>
2024-02-06 08:22:53 +0000

    update data preparation

--- code/data_engineering/clean_data.R
+++ code/data_engineering/clean_data.R
@@ -4,7 +4,7 @@ library("zoo")
 
 df<-read_csv("train_delays/data/raw/pt_results.csv")
 
-# Type Conversions and Wrong Encoding
+#### 0.Type Conversions and Wrong Encoding ####
 df$HALTESTELLEN_NAME<-gsub("Ã¼", "ü", df$HALTESTELLEN_NAME)
 df$BETRIEBSTAG<-dmy(df$BETRIEBSTAG)
 df$ANKUNFTSZEIT<-dmy_hm(df$ANKUNFTSZEIT)
@@ -15,8 +15,15 @@ df$AB_PROGNOSE<-dmy_hms(df$AB_PROGNOSE)
 
 df$FAELLT_AUS_TF<-if_else(is.na(df$AN_PROGNOSE), TRUE, df$FAELLT_AUS_TF) # set all trains without data to cancelled
 
+# accound for shadow train-IDs
+df$trainNr<-if_else(df$LINIEN_ID>10000, df$LINIEN_ID-10000, df$LINIEN_ID)
 
-# Create New Variables & Filter
+high_train_nr<-df%>%group_by(HALTESTELLEN_NAME, BETRIEBSTAG, trainNr)%>%summarise(count=n())
+df<-left_join(df, high_train_nr)
+df<-df%>%filter(!(LINIEN_ID<10000 & count>1))%>%select(-"LINIEN_ID")
+
+
+#### 1.Create New Variables & Filter ####
 df$delay<-as.numeric(df$AN_PROGNOSE-df$ANKUNFTSZEIT)/60
 df$yearmon<-as.yearmon(df$BETRIEBSTAG)
 
@@ -24,25 +31,37 @@ df<-df%>%filter(BETRIEBSTAG>='2020-12-15') # no data before time table change re
 df<-df%>%filter(HALTESTELLEN_NAME=="Zürich HB")
 
 
-# Quality Checks
+#### 2.Quality Checks ####
 days_data<-data.frame(day=unique(df$BETRIEBSTAG))
 days_sequ<-data.frame(day=seq(min(df$BETRIEBSTAG), max(df$BETRIEBSTAG), "days"))
 setdiff(days_sequ, days_data)
 
-# Explanatory Analysis
 
-plot(df%>%group_by(yearmon)%>%summarise(count=n()), type="l")
+#### 3.Explanatory Analysis ####
 
-df$delay<-as.numeric(df$AN_PROGNOSE-df$ANKUNFTSZEIT)/60
-df$yearmon<-as.yearmon(df$BETRIEBSTAG)
+### How many trains are running per period?
+df%>%group_by(yearmon)%>%summarise(count=n())
+plot(df%>%group_by(yearmon)%>%summarise(count=n()),type="l")
 
-plot(df%>%group_by(yearmon)%>%summarise(avg_delay=mean(delay, na.rm=TRUE)), type="l")
 
-df$AN_PROGNOSE
-
+### Average delay per period
+plot(df%>%group_by(BETRIEBSTAG)%>%summarise(avg_delay=mean(delay, na.rm=TRUE)), type="l")
 
 
 plot(df%>%group_by(yearmon)%>%summarise(count=n()), type="l", ylim=c(0,255))
-lines(df%>%filter(FAELLT_AUS_TF==TRUE)%>%group_by(yearmon)%>%summarise(cancelled=n()))
+lines(df%>%filter(FAELLT_AUS_TF==TRUE)%>%group_by(yearmon)%>%summarise(cancelled=n()), col="red")
+
+df%>%group_by(yearmon, trainNr)%>%summarise(count=n())%>%ggplot()+
+  geom_point(aes(x=yearmon, y=count, group=as.character(trainNr),  color=as.character(trainNr)), position=position_jitter(h=0.15,w=0.15))
+
+cancelled_daily<-left_join(days_sequ,
+                           df%>%filter(FAELLT_AUS_TF==TRUE)%>%group_by(BETRIEBSTAG)%>%summarise(cancelled=n()), 
+                           by=c("day"="BETRIEBSTAG"))%>%replace(is.na(.), 0)
+
+
+plot(cancelled_daily,ylim=c(0,7),type="l")
+lines(df%>%group_by(BETRIEBSTAG)%>%summarise(count=n()), type="l", col="red")
+
 
 plot(df%>%filter(FAELLT_AUS_TF==TRUE)%>%group_by(yearmon)%>%summarise(cancelled=n()))
+
