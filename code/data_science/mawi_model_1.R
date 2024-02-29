32e2cc9c30fc545e43549ac3bb22b458360669d1
MatthiasBFS <matthias.widmer@bfs.admin.ch>
2024-02-29 15:30:13 +0000

    work on models

--- code/data_science/mawi_model_1.R
+++ code/data_science/mawi_model_1.R
@@ -23,6 +23,8 @@ df_190<-df_190%>%
       group_by(BETRIEBSTAG)%>%
       summarise(number_trains=n()))
 
+df_190<-df_190%>%arrange(BETRIEBSTAG)
+
 # Check Data
 
 hist(df_190$delay, breaks=10)
@@ -38,12 +40,11 @@ qqnorm(df_190$delay)
 f_collect_results <- function(df, mod){
   output<-list(
     deparse(substitute(mod)),
-    format(summary(mod)$formula),
     summary(mod)$dev.expl, 
     summary(mod)$r.sq)
   
   df_results<-data.frame(output)
-  colnames(df_results)<-c("model", "formula", "dev.epxl","rsq")
+  colnames(df_results)<-c("model", "dev.epxl","rsq")
   
   
   pred <- predict(mod, df)
@@ -52,6 +53,11 @@ f_collect_results <- function(df, mod){
   
   return(list(df_results, pred))
   
+}
+
+p<-function(df, res){
+  plot(df$BETRIEBSTAG, df$delay, col="red")
+  lines(df$BETRIEBSTAG, res[[2]]$pred)
 }
 
 #### M1.1 Based On Time Index Only ####
@@ -64,28 +70,41 @@ summary(mod1.1)
 plot(mod1.1,scale=0)
 plot(resid(mod1.1))
 
-plot(df_190$BETRIEBSTAG, df_190$delay, col="red")
-lines(df_190$BETRIEBSTAG, res1.1[[2]]$pred)
+p(df_190, res1.1)
 
 
-#### M1.2 Based On Index Only ####
+#### M1.2 Based On Time Only ####
 
-mod1.2 <- gam(delay ~ s(t, fx=FALSE, k=40) + s(yday, fx=FALSE) + as.factor(month) + as.factor(wday)+number_trains, family=gaussian, data=df_190, na.action=na.omit)
+mod1.2 <- gam(delay ~ s(t, fx=FALSE, k=40) + s(yday, fx=FALSE) + as.factor(month) + as.factor(wday), family=gaussian, data=df_190, na.action=na.omit)
+
+res1.2<-f_collect_results(df_190,mod1.2)
 
 summary(mod1.2)
 plot(mod1.2,scale=0)
 plot(resid(mod1.2))
 
+p(df_190, res1.2)
 
-#### M2 Add Year ####
 
-mod2 <- gam(delay ~ s(t, bs = "ts", fx=FALSE, k=40) + s(yday, bs = "ts", fx=FALSE) + as.factor(month) + as.factor(wday)+number_trains, family=gaussian, data=df_190, na.action=na.omit)
+#### M1.3 Based On Additional Input ####
 
-summary(mod2)
-plot(mod2,scale=0)
-plot(resid(mod2))
+mod1.3 <- gam(delay ~ s(t, fx=FALSE, k=40) + s(yday, fx=FALSE) + as.factor(month) + as.factor(wday)+number_trains+holiday_zurich+holiday_munich+zh_school_h+mc_school_h+temp_max+temp_min, family=gaussian, data=df_190, na.action=na.omit)
 
-predict2<-predict(mod2, df_190)
+res1.3<-f_collect_results(df_190,mod1.3)
 
-plot(df_190$BETRIEBSTAG, df_190$delay, col="red")
-lines(df_190$BETRIEBSTAG, predict2)
+summary(mod1.3)
+plot(mod1.2,scal3=0)
+plot(resid(mod1.2))
+
+p(df_190, res1.3)
+
+
+#### M1 Let's look at results ####
+
+results<-rbind(res1.1[[1]], res1.2[[1]], res1.3[[1]])
+
+plot(factor(results$model),results$rsq)
+title("Rsquared")
+
+plot(factor(results$model),results$dev.epxl)
+title("Deviance Explained")
